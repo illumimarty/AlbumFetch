b94fceda3eff3b8b3cb8caa665ba2cf41b2cd59a
Marthen Nodado <projects@mnodado.com>
2024-03-28 15:20:33 -0700

    add project files

---
+++ app/src/main/java/com/example/albumfetch/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.albumfetch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
